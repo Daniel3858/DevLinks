4be844f0b5bfd5d73e3dea234dabaa14c012eb5a
Daniel da Silva Albuquerque <danieldasilvaalbuquerque3@gmail.com>
2023-10-30 19:54:58 -0300

    adding API consumption

--- script.js
+++ script.js
@@ -5,13 +5,23 @@ button.onclick = function toggleMode() {
   const html = document.documentElement
   html.classList.toggle('light')
 
-  const img = document.querySelector("#profile img")
+ /* const img = document.querySelector("#profile img")
 
   if (html.classList.contains('light')) {
       img.setAttribute('src', './assets/avatar-light.png')
   } else {
       img.setAttribute('src', './assets/avatar.png')
-  }
+  } */
 
+} 
+
+const url = "https://api.github.com/users/Daniel3858"
+
+async function getApi() {
+  const result = await axios.get(url)
+  const data = await result.data
+  userName.textContent = "@" + data.login
+  userAvatar.src = data.avatar_url
 }
 
+getApi()
